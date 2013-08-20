740fb05eb913ab6fb2f31a05618ffcf34c1e0b8e
Oscar Suro <outercript@gmail.com>
2013-08-20 09:40:22 -0500

    Added the controles test project
    	Just a quick review on how to use controls

---
+++ controles/controles/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  controles
+//
+//  Created by Administrador on 8/20/13.
+//  Copyright (c) 2013 Administrador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ controles/controles/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  controles
+//
+//  Created by Administrador on 8/20/13.
+//  Copyright (c) 2013 Administrador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
