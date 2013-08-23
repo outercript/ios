f8a4dee3a73657b842bf72479665240db6aef0af
Oscar Suro <outercript@gmail.com>
2013-08-23 09:42:55 -0500

    Added new project with textFields

---
+++ textField/textField/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  textField
+//
+//  Created by Administrador on 8/23/13.
+//  Copyright (c) 2013 Administrador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ textField/textField/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  textField
+//
+//  Created by Administrador on 8/23/13.
+//  Copyright (c) 2013 Administrador. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
