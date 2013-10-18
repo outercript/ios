1e3a5b09d2660d85c7bfa625da8ac13dd40d5542
Oscar Suro <outercript@gmail.com>
2013-10-18 09:45:28 -0500

    Add gestures, handlers. etc
    Signed-off-by: Oscar Suro <outercript@gmail.com>

--- MapStuff/MapStuff/ViewController.m
+++ MapStuff/MapStuff/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
 @interface ViewController ()
 
@@ -26,4 +27,56 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+#pragma mark - MapView Delegates
+-(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation{
+    //[mapView setCenterCoordinate:userLocation.coordinate animated:YES];
+    MKCoordinateRegion region = MKCoordinateRegionMake(userLocation.coordinate,
+                                                       MKCoordinateSpanMake(0.025, 0.025));
+    [mapView setRegion:region animated:YES];
+}
+
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation{
+    // Prevent modifiying user location pin
+    if([annotation isKindOfClass:[MKUserLocation class]])
+        return nil;
+
+    MKPinAnnotationView *pin = (MKPinAnnotationView *)[mapView dequeueReusableAnnotationViewWithIdentifier:@"pin"];
+    if(pin == nil){
+        pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"pin"];
+    }
+
+    // Set pin properties
+    pin.annotation = annotation;
+    pin.pinColor = MKPinAnnotationColorGreen;
+    pin.animatesDrop = YES;
+    pin.canShowCallout = YES;
+
+    // Add button to the right
+    pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    
+    return pin;
+}
+
+- (IBAction)annotationHandler:(UILongPressGestureRecognizer *)sender {
+    // Prevents the ction being shown multiple times
+    if(sender.state != UIGestureRecognizerStateBegan){
+        return;
+    }
+
+    // Convert the location of the touch event into map cordinates
+    CGPoint point = [sender locationInView:self.myMap];
+    CLLocationCoordinate2D location = [self.myMap convertPoint:point
+                                          toCoordinateFromView:self.myMap];
+
+    // Create a new annotation
+    MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+    annotation.coordinate = location;
+    annotation.title = @"Titulo";
+    annotation.subtitle = @"Aqui";
+
+    // Add it to the map
+    [self.myMap addAnnotation:annotation];
+}
 @end
