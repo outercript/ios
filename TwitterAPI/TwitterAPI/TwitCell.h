f01e95ecf5e826f21072dd5a0670e67b33ed0d9f
Oscar Suro <outercript@gmail.com>
2013-11-23 00:21:50 -0600

    Added custom Cell classes for the Twitts
    Signed-off-by: Oscar Suro <outercript@gmail.com>

---
+++ TwitterAPI/TwitterAPI/TwitCell.h
@@ -0,0 +1,13 @@
+//
+//  TwitCell.h
+//  TwitterAPI
+//
+//  Created by Oscar Suro on 11/22/13.
+//  Copyright (c) 2013 Oscar Suro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwitCell : UITableViewCell
+
+@end
---
+++ TwitterAPI/TwitterAPI/TwitCell.m
@@ -0,0 +1,29 @@
+//
+//  TwitCell.m
+//  TwitterAPI
+//
+//  Created by Oscar Suro on 11/22/13.
+//  Copyright (c) 2013 Oscar Suro. All rights reserved.
+//
+
+#import "TwitCell.h"
+
+@implementation TwitCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
